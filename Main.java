d1cc3df08059a972c3b1ee76c0e1c0598bb9ebd1
Vinay-webDev <justchatting3000@gmail.com>
2025-01-22 14:32:10 +0530

    #1

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 10;
+        boolean isPassed = true;
+        double price = 10.99;
+        char letter = 'A';
+        String name = "Bro";
+        System.out.println(letter);
+        System.out.println(num);
+        System.out.println(isPassed);
+        System.out.println(price);
+        System.out.println(name);
+    }
+}
+
