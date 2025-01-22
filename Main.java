ecc5f91714af409ab7cd0ad15a07251b4df99042
Vinay-webDev <justchatting3000@gmail.com>
2025-01-22 15:25:02 +0530

    #4

--- Main.java
+++ Main.java
@@ -1,13 +1,24 @@
 public class Main {
+    public static int findMissingNumber(int[] arr) {
+         /*find the missing number */
+         int n = arr.length + 1;
+         int current_sum = 0;
+         int actual_sum = 0;
+         for (int i = 0; i < arr.length; i++) {
+             current_sum += arr[i];
+         }
+         for (int j = 1; j <= n; j++) {
+             actual_sum += j;
+         }
+         return actual_sum - current_sum;
+    }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 4, 9, 3};
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        System.out.println(min);
+       int[] arr1 = {1, 2, 3, 4, 6};
+       int[] arr2 = {1, 2, 3, 5, 6};
+       int[] arr3 = {1, 2, 4, 5, 6};
+       System.out.println(findMissingNumber(arr1)); //5
+       System.out.println(findMissingNumber(arr2)); //4
+       System.out.println(findMissingNumber(arr3)); //3
     }
 }
-//1
+//5
