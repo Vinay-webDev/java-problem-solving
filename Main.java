b9b00658663a37fd588314ebe9e15143d00a7a90
Vinay-webDev <justchatting3000@gmail.com>
2025-01-22 14:53:14 +0530

    #3

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 4, 5, 8};
-        int max = Integer.MIN_VALUE;
+        int[] arr = {1, 2, 3, 4, 4, 9, 3};
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        System.out.println(max);
+        System.out.println(min);
     }
 }
-
+//1
