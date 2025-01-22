13b9b43972dd452fbf3352d1080add8f76406671
Vinay-webDev <justchatting3000@gmail.com>
2025-01-22 14:50:29 +0530

    #2

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int num = 10;
-        boolean isPassed = true;
-        double price = 10.99;
-        char letter = 'A';
-        String name = "Bro";
-        System.out.println(letter);
-        System.out.println(num);
-        System.out.println(isPassed);
-        System.out.println(price);
-        System.out.println(name);
+        int[] arr = {1, 2, 3, 4, 4, 5, 8};
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
     }
 }
 
