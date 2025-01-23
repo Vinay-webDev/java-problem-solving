475c1a832afc0b82cc78b39b4747bf1b54b5e6aa
Vinay-webDev <justchatting3000@gmail.com>
2025-01-23 23:35:58 +0530

    #15

---
+++ Maximumproduct.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class Maximumproduct {
+    public static int maximumProduct(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        int maxProduct = arr[n - 2] * arr[n - 1];
+        return maxProduct;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = {2, 3, 8, 4};
+        int[] arr2 = {8, 9, 1, 11, 35};
+        System.out.println(maximumProduct(arr1)); //32
+        System.out.println(maximumProduct(arr2)); //385
+    }
+}
